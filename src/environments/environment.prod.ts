ba4acb0a1024032eafa90d1ead2f8d9e2fa01477
diegofernandodev <dieferv@gmail.com>
2024-04-22 15:31:48 -0500

    se ajusta ip interna ec2 http://172.31.17.80:4000/

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/',
+  apiUrl: 'http://172.31.17.80:4000/',
   // firebaseConfig: {
   //   apiKey: '...',
   //   ...
