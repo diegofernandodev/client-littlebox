b49b2b2e860ae62585fff0b4a62034291a35681a
diegofernandodev <dieferv@gmail.com>
2024-04-22 19:18:43 -0500

    se ajusta ip public

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.31.17.80:4000/',
+  apiUrl: 'http://3.140.230.73:4000/',
   // firebaseConfig: {
   //   apiKey: '...',
   //   ...
